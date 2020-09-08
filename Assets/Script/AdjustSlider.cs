384cc6ed1cc861e88d9b4ae4c63cbcf4fc692637
DeepanshuManocha <dmanocha1509@gmail.com>
2020-09-08 13:49:35 +0530

    Added: Rotaion via slider

---
+++ Assets/Script/AdjustSlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustSlider : MonoBehaviour
+{
+    public GameObject objectToRotate;
+    public Slider slider;
+
+    private float previousValue;
+
+    void Awake()
+    {
+        this.slider.onValueChanged.AddListener(this.OnSliderChanged);
+
+        this.previousValue = this.slider.value;
+    }
+
+    void OnSliderChanged(float value)
+    {
+        float delta = value - this.previousValue;
+        this.objectToRotate.transform.Rotate(Vector3.up * delta * 360);
+
+        this.previousValue = value;
+    }
+}
